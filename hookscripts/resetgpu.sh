79a59e2ed9d4fb3680acf9a1496ef4c09a9c4ea4
John Mclaren <john@johnscs.com>
2021-05-16 22:12:40 -0400

    Update resetgpu.sh

--- hookscripts/resetgpu.sh
+++ hookscripts/resetgpu.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
-#Gives GPU back to host after a VM is shutdown
+# Gives GPU back to host after a VM is shutdown
+# Note that Proxmox has an issue where on Windows guest (maybe other) a shutdown from within the guest will not fire post-stop hook scripts
 
 if [ $2 = "post-stop" ]; then
   echo [post-stop] start
   PCIE=$(grep -oP '(?<=hostpci0: ).*?(?=,)' /etc/pve/nodes/`hostname -s`/qemu-server/$1.conf)
   echo -n 0000:$PCIE.0 > /sys/bus/pci/drivers/vfio-pci/unbind
 
-  #The specific driver you want to use need to be specifed, nouveau for nvidia usualy
+  # The specific driver you want to use need to be specifed, nouveau for nvidia usualy
   echo -n 0000:$PCIE.0 > /sys/bus/pci/drivers/nouveau/bind 
 
   # Alternative method if I can figure out how to prevent vfio-pci from automaticly binding, 
