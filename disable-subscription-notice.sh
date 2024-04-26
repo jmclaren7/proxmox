4d368b3e2218ce1f5635330ded0ba8cca74b9099
John McLaren <john@johnscs.com>
2024-04-26 14:28:33 -0400

    Update disable-subscription-notice.sh

--- disable-subscription-notice.sh
+++ disable-subscription-notice.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 # Removes the "You do not have a valid subscription for this server" popup message while logging in
+# https://johnscs.com/remove-proxmox51-subscription-notice/
+#
+# Manual steps:
+#   cd /usr/share/javascript/proxmox-widget-toolkit
+#   cp proxmoxlib.js proxmoxlib.js.bak
+#   nano proxmoxlib.js
+#   search for "function(orig_cmd) {" and add "orig_cmd();" and "return;" just after it
+#   systemctl restart pveproxy.service
 
 sed -Ezi.bak "s/(function\(orig_cmd\) \{)/\1\n\torig_cmd\(\);\n\treturn;/g" /usr/share/javascript/proxmox-widget-toolkit/proxmoxlib.js && systemctl restart pveproxy.service
 
-# cd /usr/share/javascript/proxmox-widget-toolkit
-# cp proxmoxlib.js proxmoxlib.js.bak
-# nano proxmoxlib.js
-# search for "function(orig_cmd) {" and add "orig_cmd();" and "return;" just after it
-# systemctl restart pveproxy.service
+
