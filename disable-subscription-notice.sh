72dd4a4dc36b78ab118a3e02e95c6d0a8d9acb0b
John McLaren <john@johnscs.com>
2025-07-25 23:44:50 -0400

    Update disable-subscription-notice.sh
    allow for space after 'function'

--- disable-subscription-notice.sh
+++ disable-subscription-notice.sh
@@ -6,9 +6,8 @@
 #   cd /usr/share/javascript/proxmox-widget-toolkit
 #   cp proxmoxlib.js proxmoxlib.js.bak
 #   nano proxmoxlib.js
-#   search for "function(orig_cmd) {" and add "orig_cmd();" and "return;" just after it
+#   search for "function (orig_cmd) {"  (no space after function before 8.4.5)
+#   add "orig_cmd();" and "return;" below the function declaration and save
 #   systemctl restart pveproxy.service
 
-sed -Ezi.bak "s/(function\(orig_cmd\) \{)/\1\n\torig_cmd\(\);\n\treturn;/g" /usr/share/javascript/proxmox-widget-toolkit/proxmoxlib.js && systemctl restart pveproxy.service
-
-
+sed -Ezi.bak "s/(function ?\(orig_cmd\) \{)/\1\n\torig_cmd\(\);\n\treturn;/g" /usr/share/javascript/proxmox-widget-toolkit/proxmoxlib.js && systemctl restart pveproxy.service
